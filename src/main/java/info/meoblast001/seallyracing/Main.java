a6ebefc1d7e14e4d3eeeba2ecdf9a8be96b877ec
Braden Walters <vc@braden-walters.info>
2017-03-18 19:08:27 +0100

    Missing documentation

--- src/main/java/info/meoblast001/seallyracing/Main.java
+++ src/main/java/info/meoblast001/seallyracing/Main.java
@@ -10,12 +10,23 @@ package info.meoblast001.seallyracing;
 import com.jme3.app.SimpleApplication;
 import info.meoblast001.seallyracing.states.PlayState;
 
+/**
+ * Main application class.
+ */
 public class Main extends SimpleApplication {
+  /**
+   * Application entrance.
+   * @param args Command line arguments.
+   */
   public static void main(String[] args) {
     Main app = new Main();
     app.start();
   }
 
+  /**
+   * Initialise the game.
+   * @see SimpleApplication#simpleInitApp()
+   */
   @Override
   public void simpleInitApp() {
     this.flyCam.setEnabled(false);
