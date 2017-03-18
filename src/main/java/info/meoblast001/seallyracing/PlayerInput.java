dfdb69f8db8e349696fa423e7374329fe15fa172
Braden Walters <vc@braden-walters.info>
2017-03-18 19:07:56 +0100

    Player control through input

---
+++ src/main/java/info/meoblast001/seallyracing/PlayerInput.java
@@ -0,0 +1,48 @@
+package info.meoblast001.seallyracing;
+
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.scene.Spatial;
+import info.meoblast001.seallyracing.states.PlayState;
+
+/**
+ * Collect player input and respond to it.
+ */
+public class PlayerInput implements AnalogListener, ActionListener {
+  // Movement directions.
+  public static final String MOVE_LEFT = "Left";
+  public static final String MOVE_RIGHT = "Right";
+  public static final String MOVE_UP = "Up";
+  public static final String MOVE_DOWN = "Down";
+
+  // Player character.
+  private Spatial player;
+
+  public PlayerInput(Spatial player) {
+    this.player = player;
+  }
+
+  /**
+   * Handle input which triggers a single aaction.
+   * @see ActionListener#onAction(String, boolean, float)
+   */
+  public void onAction(String name, boolean keyPressed, float tpf) {
+    // Currently no actions.
+  }
+
+  /**
+   * Handle input which may have intensity and occurs over multiple frames.
+   * @see AnalogListener#onAnalog(String, float, float)
+   */
+  public void onAnalog(String name, float value, float tpf) {
+    if (name.equals(MOVE_LEFT)) {
+      player.rotate(0.0f, PlayState.PLAYER_TORQUE * tpf, 0.0f);
+    } else if (name.equals(MOVE_RIGHT)) {
+      player.rotate(0.0f, -PlayState.PLAYER_TORQUE * tpf, 0.0f);
+    } else if (name.equals(MOVE_UP)) {
+      player.rotate(PlayState.PLAYER_TORQUE * tpf, 0.0f, 0.0f);
+    } else if (name.equals(MOVE_DOWN)) {
+      player.rotate(-PlayState.PLAYER_TORQUE * tpf, 0.0f, 0.0f);
+    }
+  }
+}
--- src/main/java/info/meoblast001/seallyracing/states/PlayState.java
+++ src/main/java/info/meoblast001/seallyracing/states/PlayState.java
@@ -4,6 +4,9 @@ import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -12,20 +15,26 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import info.meoblast001.seallyracing.FollowCamera;
+import info.meoblast001.seallyracing.PlayerInput;
 
 /**
  * AppState for game play.
  */
 public class PlayState extends AbstractAppState {
-  private final float PLAYER_SPEED = 12f;
-  private final float PLAYER_TORQUE = 0.5f;
-  private final float CAMERA_DISTANCE = 10f;
+  public static final float PLAYER_SPEED = 12f;
+  public static final float PLAYER_TORQUE = 0.5f;
+  public static final float CAMERA_DISTANCE = 10f;
 
   private SimpleApplication app;
   private Node rootNode;
   private Spatial player;
   private FollowCamera followCamera;
 
+  /**
+   * Initialise play state.
+   * @param stateManager The application state manager.
+   * @param app The game application object.
+   */
   @Override
   public void initialize(AppStateManager stateManager, Application app) {
     super.initialize(stateManager, app);
@@ -53,8 +62,24 @@ public class PlayState extends AbstractAppState {
     // Create the camera attached to the player.
     followCamera = new FollowCamera(this.app.getCamera(), this.player,
                                     CAMERA_DISTANCE);
+
+    // Capture input to player.
+    InputManager input = app.getInputManager();
+    String[] mappingNames = new String[] { PlayerInput.MOVE_LEFT,
+      PlayerInput.MOVE_RIGHT, PlayerInput.MOVE_UP, PlayerInput.MOVE_DOWN };
+    input.addListener(new PlayerInput(this.player),
+                      mappingNames);
+    input.addMapping(PlayerInput.MOVE_LEFT, new KeyTrigger(KeyInput.KEY_LEFT));
+    input.addMapping(PlayerInput.MOVE_RIGHT,
+                     new KeyTrigger(KeyInput.KEY_RIGHT));
+    input.addMapping(PlayerInput.MOVE_UP, new KeyTrigger(KeyInput.KEY_UP));
+    input.addMapping(PlayerInput.MOVE_DOWN, new KeyTrigger(KeyInput.KEY_DOWN));
   }
 
+  /**
+   * Update for next frame.
+   * @param tpf Milliseconds since last frame.
+   */
   @Override
   public void update(float tpf) {
     super.update(tpf);
@@ -63,7 +88,6 @@ public class PlayState extends AbstractAppState {
     followCamera.update();
 
     // Update player character.
-    player.rotate(-PLAYER_TORQUE * tpf, 0.0f, 0.0f);
     Vector3f forward = player.getLocalRotation().mult(Vector3f.UNIT_Z).
       mult(-PLAYER_SPEED * tpf);
     player.move(forward);
